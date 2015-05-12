6a1e89c06072c46ec96f0b8c1aadcce8a3c875a6
kimihub <kimhimitsu@gmail.com>
2015-05-12 18:49:34 +0200

    fix clean url, add req.body secure

--- router.js
+++ router.js
@@ -22,16 +22,29 @@ module.exports.send = function Router (req, auth, next) {
 
   var next = ((typeof next !== "function")? (function(data){}):next);
 
-  // parse url
-  if ((typeof req.url === 'undefined') || (typeof req.url !== 'String') || (typeof req.method === 'undefined') || (typeof req.method !== 'String'))
+  // url and method must be defined
+  if ((typeof req.url === 'undefined') || (typeof req.url !== 'string') || (typeof req.method === 'undefined') || (typeof req.method !== 'string'))
     return next(errors.NotFound());
+
+  // parse url
   var parsedUrl = require('url').parse(req.url).pathname;
-  // protect private methods, clean query string, hash url
-  req.hashUrl = parsedUrl.replace(/_/,'').trim('/').split('/');
+  // trim parsedUrl
+  if (parsedUrl.charAt(0) == '/')
+    parsedUrl = parsedUrl.substr(1, parsedUrl.length-1);
+  if (parsedUrl.charAt(parsedUrl.length-1) == '/')
+    parsedUrl = parsedUrl.substr(0, parsedUrl.length-1);
+
+  // hash parsedUrl
+  req.hashUrl = parsedUrl.split('/');
+
+  // get route
   var route = req.hashUrl.shift();
-  var method = req.method.toLowerCase();
+
+  // get method / protect private methods
+  var method = req.method.replace(/_/,'').toLowerCase();
+
   // prepare body
-  if (!req.body || typeof req.body !== 'object')
+  if (!req.body || typeof req.body !== 'object' || (typeof req.body === 'object') && Array.isArray(req.body))
     req.body = {};
 
   if (typeof routes[route] === 'undefined' || typeof routes[route][method] === 'undefined')
