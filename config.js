d0683e26b2d929bc8c4e47711be7a09253b8073d
kimihub <kimhimitsu@gmail.com>
2015-06-06 19:20:17 +0200

    delete sendmail port

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ var config = {
 	host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
 	port: {
 		http: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-		ws: 8000,
-		sendmail: 587
+		ws: 8000
 	}
 };
 
