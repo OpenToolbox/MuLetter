942b7d7f10c534461a15719c2350ed1dcf7ce319
kimihub <kimhimitsu@gmail.com>
2015-06-07 02:09:35 +0200

    port 80

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
 	debug: 0,
 	host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
 	port: {
-		http: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+		http: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 80,
 		ws: 8000
 	}
 };
