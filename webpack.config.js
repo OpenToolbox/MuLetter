42a78b033590403771bf086b7619e0da3171a4fb
kimi <kimhimitsu@gmail.com>
2016-04-22 17:38:09 +0200

    copy electron app files from src and bundle react components

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,38 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin'), CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-      bundle: ['./render.js', './style.css']
+      bundle: [
+        './assets/style.css',
+        './react/index'
+      ]
     },
-    context: __dirname + '/',
+    context: __dirname + '/src',
     output: {
-        path: __dirname + '/dist/web/',
-        filename: 'bundle.js'
+        path: __dirname + '/app',
+        filename: './[name].js'
+    },
+    resolve: {
+      extensions: ['', '.js']
     },
     module: {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
-            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel', query: { presets: ['es2015', 'react'] } }
+            { test: /\.js?$/, exclude: /(node_modules)/, loader: 'babel', query: { presets: ['es2015', 'react'] } }
         ]
     },
+    presets: [
+        'es2015-webpack',
+        'react',
+    ],
     plugins: [
         new HtmlWebpackPlugin({
-          filename: 'index.html',
-          templateContent: '<head><title>MuLetter</title></head><body><div id=root></div></body>',
-          inject: true
-        })
+          filename: './index.html',
+          templateContent: "<div id=root></div><script src=bundle.js></script>",
+          inject: false
+        }),
+        new CopyWebpackPlugin([
+           { from: 'electron/main.js', to: 'main.js' }
+        ])
     ]
 };
